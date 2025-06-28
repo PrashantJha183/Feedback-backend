460822738dfb0423f4a92a2fde8806827b010b7a
Prashant Jha <jhap5754@gmail.com>
2025-06-28 21:27:35 +0530

    Updated cors policy again

--- Server/app/main.py
+++ Server/app/main.py
@@ -11,7 +11,9 @@ app.add_middleware(
     allow_origins=[
         "http://localhost",
         "http://localhost:5173",
-        "https://127.0.0.1:5173"
+        "https://127.0.0.1:5173",
+        "http://localhost:3000",
+        "https://127.0.0.1:3000"
     ],
     allow_credentials=True,
     allow_methods=["*"],
